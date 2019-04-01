d8009bdfa3ec0cda738d4598452f22ea7f83275e
tomekand1 <tomekand1@gmail.com>
2019-04-01 09:40:09 +0100

    fixed server

--- db/data/index.js
+++ db/data/index.js
@@ -1,9 +1,9 @@
-const data = { test, development, production: development };
 const test = require('./test-data');
 const development = require('./development-data');
+const data = { test, development, production: development };
 
 const env = process.env.NODE_ENV || 'development';
 
-const data = { test, development };
+//const data = { test, development };
 
 module.exports = data[env];
