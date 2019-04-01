7e71619cfcea99ad7766e03662bb983703465ab4
tomekand1 <tomekand1@gmail.com>
2019-04-01 13:35:12 +0100

    comment deleted

--- db/data/index.js
+++ db/data/index.js
@@ -5,6 +5,4 @@ const data = { test, development, production: development };
 
 const env = process.env.NODE_ENV || 'development';
 
-// const data = { test, development };
-
 module.exports = data[env];
